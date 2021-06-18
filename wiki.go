f4eeaca1a7ab24dffb5399df26e5e46ecd455c1c
Norbert Baliga <norbert.baliga@logmein.com>
2021-06-18 18:02:03 +0200

    Sixth version of wiki (renderTemplate)

--- wiki.go
+++ wiki.go
@@ -29,16 +29,27 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p interface{}) {
+	t, err := template.ParseFiles("templates/" + tmpl + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
 
 	if err != nil {
-		t, _ := template.ParseFiles("templates/error.html")
-		t.Execute(w, title)
+		renderTemplate(w, "error", title)
 	} else {
-		t, _ := template.ParseFiles("templates/view.html")
-		t.Execute(w, p)
+		renderTemplate(w, "view", p)
 	}
 }
 
@@ -50,15 +61,18 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 		p = &Page{Title: title}
 	}
 
-	t, _ := template.ParseFiles("templates/edit.html")
-	t.Execute(w, p)
+	renderTemplate(w, "edit", p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/save/"):]
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	p.save()
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
