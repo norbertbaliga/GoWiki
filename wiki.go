20c748ea9cd78552a4d175790178709bc96065bc
Norbert Baliga <norbert.baliga@logmein.com>
2021-06-25 12:44:43 +0200

    Add index page with pages listing and delete function

--- wiki.go
+++ wiki.go
@@ -8,7 +8,11 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"regexp"
+	"sort"
+	"strings"
 )
 
 type Page struct {
@@ -16,8 +20,8 @@ type Page struct {
 	Body  []byte
 }
 
-var templates = template.Must(template.ParseFiles("templates/edit.html", "templates/view.html", "templates/error.html"))
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9_-]+)$")
+var templates = template.Must(template.ParseFiles("templates/header.html", "templates/index.html", "templates/edit.html", "templates/view.html", "templates/error.html"))
+var validPath = regexp.MustCompile("^/(delete|edit|save|view)/([a-zA-Z0-9_-]+)$")
 
 func (p *Page) save() error {
 	filename := "pages/" + p.Title + ".txt"
@@ -69,6 +73,15 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+func deleteHandler(w http.ResponseWriter, r *http.Request, title string) {
+	err := os.Remove("./pages/" + title + ".txt")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
@@ -80,10 +93,52 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	}
 }
 
+func getFileInfosFromDir(dirpath string) ([]os.FileInfo, error) {
+	f, err := os.Open(dirpath)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	sort.Slice(files, func(i, j int) bool { return files[i].Name() < files[j].Name() })
+
+	return files, nil
+}
+
+func getNamesOfFileInfos(files []os.FileInfo) []string {
+	var names []string
+	for _, file := range files {
+		ext := filepath.Ext(file.Name())
+		if ext == ".txt" {
+			names = append(names, strings.TrimSuffix(file.Name(), ext))
+		}
+	}
+	return names
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fileInfos, err := getFileInfosFromDir("./pages")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	pages := getNamesOfFileInfos(fileInfos)
+	renderTemplate(w, "index", pages)
+}
+
 func main() {
+	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/delete/", makeHandler(deleteHandler))
 
 	fmt.Println("Start webserver at *:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
